8693ae0ee5db064ea753cce0ca9a609d306e68eb
codageeksdemo <support@codageeks.com>
2022-02-27 16:13:16 +0530

    transform generated code

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,10 +1,6 @@
 git add /home/miracle/work/git/communication_microservice
-git commit -m"transform generated code"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
+git commit -m "transform generated code"
+
  git status
  echo "Pushing data to remote server!!!"
- git push -u origin master
-fi
+ git push -f  -u origin master
