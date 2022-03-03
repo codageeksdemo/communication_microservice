ea2f71988b1874de89ef1df1932f0a04048a2d53
codageeksdemo <support@codageeks.com>
2022-03-03 21:22:02 +0530

    transform generated code

--- src/main/java/com/miracle/demo/communicationmicroservice/CommunicationmicroserviceApplication.java
+++ src/main/java/com/miracle/demo/communicationmicroservice/CommunicationmicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.miracle.demo.communicationmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.miracle.**")
 public class CommunicationmicroserviceApplication {
 
 	public static void main(String[] args) {
